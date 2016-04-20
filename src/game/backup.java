d4b46c58dfad36a4f79c231a7d79a5ba8ef57a69
Andrej Chudy <xchudy03@stud.fit.vutbr.cz>
2016-04-20 20:28:02 +0200

    Rename

--- src/game/backup.java
+++ src/game/backup.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * Created by andrejchudy on 19/04/16.
- */
+ */ 
 
 public class Backup implements java.io.Serializable{
 
@@ -17,7 +17,7 @@ public class Backup implements java.io.Serializable{
     public List<TurnBackUp> backupTurns;
     public Player player1;
     public Player player2;
-    public int sizeBoard;
+    public int boardSize;
 
 
     public Backup(){
@@ -39,7 +39,7 @@ public class Backup implements java.io.Serializable{
     }
 
     public void load(){
-     
+
 
 
     }
